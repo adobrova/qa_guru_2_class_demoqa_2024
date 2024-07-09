ea2ebc8f3ecaedbb03e6290b52fedd834b36c8a7
adobrova <rezolventa86@rambler.ru>
2024-07-10 03:51:16 +0500

    Added TextBoxTests with selectors

--- src/test/java/TextBoxTests.java
+++ src/test/java/TextBoxTests.java
@@ -2,9 +2,10 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.CollectionCondition.size;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
 
 public class TextBoxTests {
 
@@ -12,44 +13,85 @@ public class TextBoxTests {
     static void beforeAll(){
         Configuration.browserSize = "1980x1080";
         Configuration.baseUrl = "https://demoqa.com";
-        Configuration.holdBrowserOpen = true;
+
+        // перед коммитом удалить/закомментировать/поменять на false
+        // при удалённом запуске тестов - браузер останется висеть открытым и будет занимать ресурсы
+        //Configuration.holdBrowserOpen = true;
     }
 
 
     @Test
     void fillFormTest() {
-        System.out.println("###       first circle");
         open("/automation-practice-form");
+
+        //после открытия страницы скрываем рекламу и футер
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
+
+        //заполняем таблицу
         $("#firstName").setValue("Albina");
         $("#lastName").setValue("Dobrova");
         $("#userEmail").setValue("rezolventa86@rambler.ru");
-
-        //   $("[name=q]").setValue("selenide");
-
+        $("label[for=gender-radio-2]").click();                //здесь выбор пола
         $("#userNumber").setValue("9872552206");
-        //  $("#dateOfBirthInput").setValue("selenide");
 
-        //    $("[name=q]").setValue("selenide");
-        //    $("[name=q]").setValue("selenide");
 
-        //    $("#uploadPicture").setValue("selenide");
+     // нижеописанный метод рабочий, но на странице баг и при очищении поля календаря
+     // возникает белый экране
+     // здесь поле очищаем
+     //   $("#dateOfBirthInput").sendKeys(Keys.CONTROL + "A");
+     //   $("#dateOfBirthInput").sendKeys(Keys.BACK_SPACE);
+     // а здесь ставим свое значение
+     //   $("#dateOfBirthInput").sendKeys(Keys."02 Jan 1986");
+
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("January");
+        $(".react-datepicker__year-select").selectOption("1986");
+        $(".react-datepicker__day--002").click();
+
+
+         $("#subjectsInput").setValue("Comp").pressEnter();
+        $("label[for=hobbies-checkbox-1]").click();  //здесь выбор хобби
+        $("label[for=hobbies-checkbox-2]").click();
+
+        $("#uploadPicture").uploadFromClasspath("cat.jpg");
         $("#currentAddress").setValue("Ufa, Russia");
 
-        //   $("[name=q]").setValue("selenide");
-        //   $("[name=q]").setValue("selenide");
+        $("#state").click();
+        $("#state").$(byText("NCR")).click();
+
+        $("#city").click();
+        $("#city").$(byText("Delhi")).click();
+
+
+
+
+
+      //  $("#city").setValue("Del").pressEnter();        //выбор города в выбранном штате в индии
 
         $("#submit").click();
 
-        $("Student Name").shouldHave(text("Albina Dobrova"));
-        $("Student Email").shouldHave(text("rezolventa86@rambler.ru"));
-        $("Gender").shouldHave(text("Female"));
-        $("Mobile").shouldHave(text("9872552206"));
-        $("Date of Birth").shouldHave(text("02 January,1986"));
-        $("Subjects").shouldHave(text("qa automation"));
-        $("Hobbies").shouldHave(text("Sports, Reading"));
-        $("Picture").shouldHave(text("cat.jpg"));
-        $("Address").shouldHave(text("Ufa, Russia"));
-        $("State and City").shouldHave(text("NCR Delhi"));
+        //проверка на все заполненные поля в финальной таблице
+        //проверим, что полей всего 10
+        $$("[class~=table] tbody tr").shouldHave(size(10));
+
+        //проверка на все заполненные поля в финальной таблице
+        $(byText("Albina Dobrova")).shouldBe(visible);
+        $(byText("rezolventa86@rambler.ru")).shouldBe(visible);
+        $(byText("Female")).shouldBe(visible);
+        $(byText("9872552206")).shouldBe(visible);
+        $(byText("02 January,1986")).shouldBe(visible);
+        $(byText("Computer Science")).shouldBe(visible);
+        $(byText("Sports, Reading")).shouldBe(visible);
+        $(byText("cat.jpg")).shouldBe(visible);
+        $(byText("Ufa, Russia")).shouldBe(visible);
+        $(byText("NCR Delhi")).shouldBe(visible);
+
+
+
+
+
+
     }
 
 
